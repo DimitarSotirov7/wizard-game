dfb7d331feba50aba9bf1020fce138dafe7cdea0
DimitarSotirov7 <dimitarsotirov7@gmail.com>
2020-10-27 19:30:05 +0200

    Add Collision Logic

--- src/app.js
+++ src/app.js
@@ -156,11 +156,10 @@ function solve() {
         let firstRect = firstElement.getBoundingClientRect();
         let secondRect = secondElement.getBoundingClientRect();
 
-        return !(firstRect.top > secondRect.botton ||
-            firstRect.botton < secondRect.top ||
+        return !(firstRect.top > secondRect.bottom ||
+            firstRect.bottom < secondRect.top ||
             firstRect.right < secondRect.left ||
             firstRect.left > secondRect.right);
-
     }
 
     function addFireBall(player) {
